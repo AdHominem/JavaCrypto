54a97eea3bd0e4b369d5d461404b7c6ee361ace9
jk <vargsmane@gmx.de>
2017-08-23 22:51:47 +0200

    Replaced usages of byte with int due to Java primitives design

--- src/Main.java
+++ src/Main.java
@@ -1,20 +1,22 @@
 import javax.crypto.KeyGenerator;
 import javax.crypto.SecretKey;
 import java.security.NoSuchAlgorithmException;
+import java.util.Arrays;
 
 public class Main {
 
     public static void main(final String[] args) throws InterruptedException, NoSuchAlgorithmException {
 
-        System.out.println(CryptoUtil.MD5("This is a test!"));
+//        System.out.println(CryptoUtil.MD5("This is a test!"));
+//
+//        final KeyGenerator keyGen = KeyGenerator.getInstance("HmacMD5");
+//        final SecretKey MD5key = keyGen.generateKey();
+//
+//        System.out.println(CryptoUtil.HMAC_MD5("This is a test!", MD5key));
 
-        final KeyGenerator keyGen = KeyGenerator.getInstance("HmacMD5");
-        final SecretKey MD5key = keyGen.generateKey();
-
-        System.out.println(CryptoUtil.HMAC_MD5("This is a test!", MD5key));
-
-//        byte[] cipher = Crypto.xor("Hello my 3 mates!".getBytes(), 13);
-//        Crypto.crackXor(cipher).forEach((x,y) -> System.out.printf("%s:\t%d\n", x, y));
+        int[] cipher = CryptoUtil.xor("Hello my 3 mates!", 1);
+        System.out.println(Arrays.toString(cipher));
+        CryptoUtil.crackXor(cipher).forEach((x,y) -> System.out.printf("%s:\t%d\n", x, y));
 
 //        DHParty client = new DHParty("Client", 13, 2);
 //        Thread.sleep(3000);
